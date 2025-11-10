ed87f32ead671b21648273277d09400be8ae2b6d
IlledNacu <bkilled7@gmail.com>
2025-11-10 16:00:46 -0300

    Modelo para el update limitado del cliente. Corrección de endpoints de create y update

--- endpoints/clientes.py
+++ endpoints/clientes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 import models
 import req_res_models
 import database
@@ -22,17 +23,29 @@ def get_cliente(id:str, db:Session = Depends(database.get_db)):
 def create_cliente(cliente:req_res_models.ClienteCreate, db:Session = Depends(database.get_db)):
     nuevo_cliente = models.Cliente(**cliente.model_dump())
     db.add(nuevo_cliente)
-    db.commit()
+    # db.commit()
+    try:
+        db.commit() # 🛑 El error ocurre aquí
+    except IntegrityError: # 🛑 Manejar el error si el ID ya existe
+        db.rollback()
+        raise HTTPException(status_code=400, detail="El número de cuenta (ID) ya existe.")
+
     db.refresh(nuevo_cliente)
     return nuevo_cliente
 
 @router.put("/{id}", response_model=req_res_models.ClienteResponse)
-def update_cliente(id:str, cliente:req_res_models.ClienteCreate, db:Session = Depends(database.get_db)):
+def update_cliente(id:str, cliente:req_res_models.ClienteUpdate, db:Session = Depends(database.get_db)):
     db_cliente = db.query(models.Cliente).filter(models.Cliente.id == id).first()
     if not db_cliente:
         raise HTTPException(status_code=404, detail="Este número de cuenta no existe.")
-    for field, value in cliente.model_dump().items():
+    
+    # Pydantic y model_dump(exclude_unset=True) envían solo los campos que realmente se enviaron
+    update_data = cliente.model_dump(exclude_unset=True)     
+        
+    for field, value in update_data.items(): 
         setattr(db_cliente, field, value)
+    # for field, value in cliente.model_dump().items():
+    #     setattr(db_cliente, field, value)
     db.commit()
     db.refresh(db_cliente)
     return db_cliente
